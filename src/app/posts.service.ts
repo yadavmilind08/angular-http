9b252e93a012028ccff4405142e2baa343537f49
Milind Yadav <yadavmilind08@gmail.com>
2021-04-19 21:17:16 +0530

    observing events type

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 
 import { Post } from "./post.model";
 
@@ -61,6 +61,20 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete('https://ng-complete-guide-242a4-default-rtdb.firebaseio.com/posts.json');
+    return this.http.delete
+      (
+        'https://ng-complete-guide-242a4-default-rtdb.firebaseio.com/posts.json',
+        {
+          observe: 'events'
+        }
+      ).pipe(tap(event => {
+        console.log(event);
+        if (event.type === HttpEventType.Sent) {
+          // ...
+        }
+        if (event.type === HttpEventType.Response) {
+          console.log(event.body);
+        }
+      }));
   }
 }
