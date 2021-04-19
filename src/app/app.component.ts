5355bd97daf6bcfb6735ec8ca2e653ca75d0978e
Milind Yadav <yadavmilind08@gmail.com>
2021-04-19 18:54:58 +0530

    sending a POST request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,16 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post('https://ng-complete-guide-242a4-default-rtdb.firebaseio.com/posts.json', postData)
+      .subscribe(responseData => {
+        console.log({ responseData });
+      });
   }
 
   onFetchPosts() {
