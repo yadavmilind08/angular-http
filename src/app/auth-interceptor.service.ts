54f52b599cd5187a976e4186785cb29b36f1e6f6
Milind Yadav <yadavmilind08@gmail.com>
2021-04-21 19:24:22 +0530

    Manipulating request headers

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -2,7 +2,7 @@ import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http"
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('On Request');
-    return next.handle(req);
+    const modifiedRequest = req.clone({ headers: req.headers.append('Auth', 'xyz') });
+    return next.handle(modifiedRequest);
   }
 }
